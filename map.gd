d575baa2d48ef8c89691fc451a5115a440d65a2b
SlinkMass <theo.louis1@hotmail.com>
2025-03-06 15:49:12 +0000

    Added like one movement thing

--- map.gd
+++ map.gd
@@ -7,7 +7,8 @@ var select_area = Area2D
 var moving := false
 var moving_to = null
 var moving_vector := 0.0
-var moving_speed := 0.2 #defines the speed at which the units move
+var moving_speed := 0.1 #defines the speed at which the units move
+var moving_mode := "Direct"
 
 func _ready() -> void:
 	select_area_shape = $Area2D/CollisionShape2D
@@ -20,15 +21,20 @@ func _process(_delta: float) -> void:
 func _physics_process(delta: float) -> void:
 	if moving:
 		moving_vector += delta * moving_speed
-		for unit in $enemies.get_children():
-			if unit.get("selected"):
-				unit.position = unit.position.lerp(Vector2(moving_to.x, moving_to.y), moving_vector)
+		if moving_mode == "Line":
+			var n = 0
+			for unit in $enemies.get_children():
+				if unit.get("selected"):
+					unit.position = unit.position.lerp(Vector2(moving_to.x, moving_to.y+n), moving_vector)
+					n+=20
+		elif moving_mode == "Direct":
+			for unit in $enemies.get_children():
+				if unit.get("selected"):
+					unit.position = unit.position.lerp(Vector2(moving_to.x, moving_to.y), moving_vector)
 		if moving_vector >= moving_speed:
 			moving = false
 			moving_vector = 0.0
-		
-	
-	
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("L_click"):
 		selecting = true
@@ -44,7 +50,7 @@ func _input(event: InputEvent) -> void:
 		for area in areas:
 			print(area)
 			area.get_parent().toggle_select()
-			
+	
 	if event.is_action_pressed("R_click"):
 		if !moving:
 			moving_to = get_global_mouse_position()
@@ -57,3 +63,13 @@ func _draw() -> void:
 		var mousepos = get_global_mouse_position()
 		var size = select_start - mousepos
 		draw_rect(Rect2(mousepos,size),Color.BLACK,false)
+
+
+func _on_move_line_pressed() -> void:
+	moving_mode = "Line"
+
+func _on_move_direct_pressed() -> void:
+	moving_mode = "Direct"
+
+func _on_move_keep_pressed() -> void:
+	moving_mode = "Keep"
