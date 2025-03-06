028d39229d5521be545878520ea64976241ddb3c
SlinkMass <theo.louis1@hotmail.com>
2025-03-06 10:42:10 +0000

    Added basic unit movement

--- map.gd
+++ map.gd
@@ -4,7 +4,11 @@ var selecting := false
 var select_start := Vector2(0,0)
 var select_area_shape = CollisionShape2D
 var select_area = Area2D
-# Called when the node enters the scene tree for the first time.
+var moving := false
+var moving_to = null
+var moving_vector := 0.0
+var moving_speed := 0.2 #defines the speed at which the units move
+
 func _ready() -> void:
 	select_area_shape = $Area2D/CollisionShape2D
 	select_area = $Area2D
@@ -12,6 +16,17 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	queue_redraw()
+
+func _physics_process(delta: float) -> void:
+	if moving:
+		moving_vector += delta * moving_speed
+		for unit in $enemies.get_children():
+			if unit.get("selected"):
+				unit.position = unit.position.lerp(Vector2(moving_to.x, moving_to.y), moving_vector)
+		if moving_vector >= moving_speed:
+			moving = false
+			moving_vector = 0.0
+		
 	
 	
 func _input(event: InputEvent) -> void:
@@ -27,11 +42,18 @@ func _input(event: InputEvent) -> void:
 		select_area_shape.shape.size = abs(select_start - mousepos)
 		var areas = select_area.get_overlapping_areas()
 		for area in areas:
+			print(area)
 			area.get_parent().toggle_select()
-
+			
+	if event.is_action_pressed("R_click"):
+		if !moving:
+			moving_to = get_global_mouse_position()
+			for unit in $enemies.get_children():
+				if unit.get("selected"):
+					moving = true
 
 func _draw() -> void:
 	if selecting:
 		var mousepos = get_global_mouse_position()
 		var size = select_start - mousepos
-		draw_rect(Rect2(mousepos,size),Color.BLUE,true)
+		draw_rect(Rect2(mousepos,size),Color.BLACK,false)
